5a18a96321458e4a5d6e646d50a767827ea87c16
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-04-18 23:01:05 -0300

    Added more wallpaper display enum members

--- Unsplash.Core/Wallpaper.cs
+++ Unsplash.Core/Wallpaper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -10,10 +11,6 @@ namespace Unsplash.Core
 {
     public sealed class Wallpaper
     {
-        Wallpaper()
-        {
-        }
-
         const int SPI_SETDESKWALLPAPER = 20;
         const int SPIF_UPDATEINIFILE = 0x01;
         const int SPIF_SENDWININICHANGE = 0x02;
@@ -23,42 +20,65 @@ namespace Unsplash.Core
 
         public enum Style
         {
-            Tiled,
-            Centered,
-            Stretched
+            Fit,
+            [Description("Windows 8 or newer")] Span,
+            Stretch,
+            Tile,
+            Center,
+            Fill
         }
 
         public static void Set(Uri uri, Style style)
         {
-            var s = new WebClient().OpenRead(uri.ToString());
-
-            var img = Image.FromStream(s);
-            var tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
-            img.Save(tempPath, ImageFormat.Bmp);
-
-            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-            if (style == Style.Stretched)
+            try
             {
-                key.SetValue(@"WallpaperStyle", 2.ToString());
-                key.SetValue(@"TileWallpaper", 0.ToString());
-            }
+                var s = new WebClient().OpenRead(uri.ToString());
 
-            if (style == Style.Centered)
+                var img = Image.FromStream(s);
+                var tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+                img.Save(tempPath, ImageFormat.Bmp);
+
+                var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+                if (style == Style.Fill)
+                {
+                    key.SetValue(@"WallpaperStyle", 10.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Fit)
+                {
+                    key.SetValue(@"WallpaperStyle", 6.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Span) // Windows 8 or newer only!
+                {
+                    key.SetValue(@"WallpaperStyle", 22.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Stretch)
+                {
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                if (style == Style.Tile)
+                {
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                }
+                if (style == Style.Center)
+                {
+                    key.SetValue(@"WallpaperStyle", 0.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                }
+                SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                    0,
+                    tempPath,
+                    SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+            catch
             {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
-                key.SetValue(@"TileWallpaper", 0.ToString());
+                //Supress
             }
-
-            if (style == Style.Tiled)
-            {
-                key.SetValue(@"WallpaperStyle", 1.ToString());
-                key.SetValue(@"TileWallpaper", 1.ToString());
-            }
-
-            SystemParametersInfo(SPI_SETDESKWALLPAPER,
-                0,
-                tempPath,
-                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
     }
 }
